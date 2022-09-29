217c1c4bca9d5232c1be96a0f089d82e14e67b47
Eduardo <eduardomelle@gmail.com>
2022-09-29 08:14:21 -0300

    Como construir aplicações evolutivas sem ser refém das modinhas

--- agencia-app/src/main/java/br/com/eduardomelle/cliente/ClienteResource.java
+++ agencia-app/src/main/java/br/com/eduardomelle/cliente/ClienteResource.java
@@ -2,9 +2,12 @@ package br.com.eduardomelle.cliente;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/cliente")
@@ -14,6 +17,20 @@ public class ClienteResource {
   @Produces(MediaType.APPLICATION_JSON)
   public List<Cliente> get() {
     return Cliente.listAll();
+  }
+
+  @GET
+  @Path("/findById")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Cliente findById(@QueryParam("id") long id) {
+    return Cliente.findById(id);
+  }
+
+  @DELETE
+  @Path("/deleteById")
+  @Transactional
+  public void deleteById(@QueryParam("id") long id) {
+    Cliente.deleteById(id);
   }
 
 }
